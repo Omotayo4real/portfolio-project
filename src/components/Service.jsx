a1e00932651405e12bb00153661c3847df583756
Omotayo4real <omotayoenisan9@gmail.com>
2025-08-12 17:11:11 +0100

    removed id elements

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,32 +1,26 @@
 
 const services = [
     {
-        id: 1,
         title: 'Web Design',
         description: 'Creating visually appealing, user-friendly and scalable web-design tailored to the clients need.',
     },
     {
-        id: 2,
         title: 'UI/UX Optimization',
         description: 'Improving the usability and overall user experience of websites.',
     },
     {
-        id: 3,
         title: 'Custom Web Application',
         description: 'Building dynamic, interactive applications to meet specific business.',
     },
     {
-        id: 4,
         title: 'Performance Enhancement',
         description: 'Optimizing website speed and efficiency for better performance.',
     },
     {
-        id: 5,
         title: 'Responsive Development',
         description: 'Ensuring websites look great on all devices, from desktops to smartphones.',
     },
     {
-        id: 6,
         title: 'SEO Integration',
         description: 'Implementing best practices for search engines optimization to improve the site visibility.',
     }
@@ -39,14 +33,13 @@ const Service = () => {
            <div className='container mx-auto px-8 md:px-16 lg:px-24'>
               <h2 className='text-4xl text-bold text-center mb-12'>My Services</h2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                  {services.map((service) => (
-                    <div key={service.id} className="bg-gray-800 px-6 pb-6 rounded-lg hover:shadow-lg
+                  {services.map((service, index) => (
+                    <div key={index} className="bg-gray-800 px-6 pb-6 rounded-lg hover:shadow-lg
                                                      transform transition-transform duration-300
                                                       hover:scale-105">
 
                         <div className="text-right text-2xl font-bold text-transparent bg-clip-text 
                                         bg-gradient-to-r from-green-600 to-blue-400">
-                          {service.id}
                         </div>
                         <h3 className="mt-2 text-2xl font-bold text-transparent bg-clip-text
                                        bg-gradient-to-r from-green-400 to-blue-500">
